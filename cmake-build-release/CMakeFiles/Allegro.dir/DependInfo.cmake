1a0691b430920289c48102862ec45303271518c0
onixtsm <alexander@sautkin.com>
2021-06-08 12:02:24 +0300

    create real collision

---
+++ cmake-build-release/CMakeFiles/Allegro.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/onix/works/cpp/allegro/AlegroAppp.cpp" "/home/onix/works/cpp/allegro/cmake-build-release/CMakeFiles/Allegro.dir/AlegroAppp.cpp.o"
+  "/home/onix/works/cpp/allegro/AllegroBase.cpp" "/home/onix/works/cpp/allegro/cmake-build-release/CMakeFiles/Allegro.dir/AllegroBase.cpp.o"
+  "/home/onix/works/cpp/allegro/Circle.cpp" "/home/onix/works/cpp/allegro/cmake-build-release/CMakeFiles/Allegro.dir/Circle.cpp.o"
+  "/home/onix/works/cpp/allegro/ControlablleSquare.cpp" "/home/onix/works/cpp/allegro/cmake-build-release/CMakeFiles/Allegro.dir/ControlablleSquare.cpp.o"
+  "/home/onix/works/cpp/allegro/Rect.cpp" "/home/onix/works/cpp/allegro/cmake-build-release/CMakeFiles/Allegro.dir/Rect.cpp.o"
+  "/home/onix/works/cpp/allegro/ScreenSaver.cpp" "/home/onix/works/cpp/allegro/cmake-build-release/CMakeFiles/Allegro.dir/ScreenSaver.cpp.o"
+  "/home/onix/works/cpp/allegro/Shape.cpp" "/home/onix/works/cpp/allegro/cmake-build-release/CMakeFiles/Allegro.dir/Shape.cpp.o"
+  "/home/onix/works/cpp/allegro/ShapeFactory.cpp" "/home/onix/works/cpp/allegro/cmake-build-release/CMakeFiles/Allegro.dir/ShapeFactory.cpp.o"
+  "/home/onix/works/cpp/allegro/Square.cpp" "/home/onix/works/cpp/allegro/cmake-build-release/CMakeFiles/Allegro.dir/Square.cpp.o"
+  "/home/onix/works/cpp/allegro/Vector.cpp" "/home/onix/works/cpp/allegro/cmake-build-release/CMakeFiles/Allegro.dir/Vector.cpp.o"
+  "/home/onix/works/cpp/allegro/main.cpp" "/home/onix/works/cpp/allegro/cmake-build-release/CMakeFiles/Allegro.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
