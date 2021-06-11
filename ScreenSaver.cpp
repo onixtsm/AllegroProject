1408481593ab9db11206e828a0461c2f1be712cc
onixtsm <alexander@sautkin.com>
2021-06-11 10:38:25 +0300

    added std:: to .h

--- ScreenSaver.cpp
+++ ScreenSaver.cpp
@@ -2,7 +2,6 @@
 // Created by onix on 05.06.21.
 //
 
-#include <iostream>
 #include <valarray>
 #include "ScreenSaver.h"
 #include <cmath>
@@ -10,8 +9,7 @@
 using namespace std;
 
 ScreenSaver::ScreenSaver() :
-        size_(0)
-{
+        size_(0) {
 }
 
 ScreenSaver::~ScreenSaver() {
@@ -29,14 +27,15 @@ void ScreenSaver::add(Shape *shape) {
     size_++;
 }
 
-Shape* ScreenSaver::get(int n) {
+Shape *ScreenSaver::get(int n) {
     return shapes[n];
 }
 
-Shape* ScreenSaver::getLast() {
+Shape *ScreenSaver::getLast() {
     return shapes[size_];
 }
-ScreenSaver & ScreenSaver::Instance() {
+
+ScreenSaver &ScreenSaver::Instance() {
     static ScreenSaver instance;
     return instance;
 }
@@ -44,7 +43,7 @@ ScreenSaver & ScreenSaver::Instance() {
 void ScreenSaver::next() {
     for (int i = 0; i < size_; i++) {
         shapes[i]->move();
-        for (int j = i+1; j < size_; j++) {
+        for (int j = i + 1; j < size_; j++) {
             Shape *a = shapes[i];
             Shape *b = shapes[j];
             if (overlap(a, b)) {
@@ -53,23 +52,31 @@ void ScreenSaver::next() {
                 double vb = b->vector.getSpeed();
 
                 double phy = asin((a->getVx() * b->getVy() - a->getVy() * b->getVx()) /
-                        (a->vector.getSpeed() * b->vector.getSpeed()));
+                                  (a->vector.getSpeed() * b->vector.getSpeed()));
                 double tetaA = acos(a->getVx() / va);
                 double tetaB = acos(b->getVx() / vb);
 
-                double vaxn = (va * cos(tetaA - phy) * (a->getM() - b->getM()) + 2 * b->getM() * vb * cos(tetaB - phy) * cos(phy) + va *
-                                                                                                                                           sin(tetaA - phy) *
-                                                                                                                                           cos(phy + M_PI / 2)) / (a->getM() + b->getM());
+                double vaxn = (va * cos(tetaA - phy) * (a->getM() - b->getM()) +
+                               2 * b->getM() * vb * cos(tetaB - phy) * cos(phy) + va *
+                                                                                  sin(tetaA - phy) *
+                                                                                  cos(phy + M_PI / 2)) /
+                              (a->getM() + b->getM());
 
-                double vayn = (va * cos(tetaA - phy) * (a->getM() - b->getM()) + 2 * b->getM() * vb * cos(tetaB - phy) * sin(phy) + va *
-                                                                                                                                    sin(tetaA - phy) *
-                                                                                                                                    sin(phy + M_PI / 2)) / (a->getM() + b->getM());
-                double vbyn = (vb * cos(tetaB - phy) * (b->getM() - a->getM()) + 2 * a->getM() * va * cos(tetaA - phy) * sin(phy) + vb *
-                                                                                                                                    sin(tetaB - phy) *
-                                                                                                                                    sin(phy + M_PI / 2)) / (a->getM() + b->getM());
-                double vbxn = (vb * cos(tetaB - phy) * (b->getM() - a->getM()) + 2 * a->getM() * va * cos(tetaA - phy) * cos(phy) + vb *
-                                                                                                                                    sin(tetaB - phy) *
-                                                                                                                                    cos(phy + M_PI / 2)) / (a->getM() + b->getM());
+                double vayn = (va * cos(tetaA - phy) * (a->getM() - b->getM()) +
+                               2 * b->getM() * vb * cos(tetaB - phy) * sin(phy) + va *
+                                                                                  sin(tetaA - phy) *
+                                                                                  sin(phy + M_PI / 2)) /
+                              (a->getM() + b->getM());
+                double vbyn = (vb * cos(tetaB - phy) * (b->getM() - a->getM()) +
+                               2 * a->getM() * va * cos(tetaA - phy) * sin(phy) + vb *
+                                                                                  sin(tetaB - phy) *
+                                                                                  sin(phy + M_PI / 2)) /
+                              (a->getM() + b->getM());
+                double vbxn = (vb * cos(tetaB - phy) * (b->getM() - a->getM()) +
+                               2 * a->getM() * va * cos(tetaA - phy) * cos(phy) + vb *
+                                                                                  sin(tetaB - phy) *
+                                                                                  cos(phy + M_PI / 2)) /
+                              (a->getM() + b->getM());
 
                 a->setVx(vaxn);
                 a->setVy(vayn);
@@ -84,16 +91,16 @@ void ScreenSaver::next() {
 
 void ScreenSaver::draw() {
     al_clear_to_color(BG_COLOR);
-    for ( int i = 0; i < size_; i++) {
+    for (int i = 0; i < size_; i++) {
         shapes[i]->draw();
     }
 }
 
 bool ScreenSaver::overlap(Shape *a, Shape *b) {
     if ((a->getX() - a->getW() / 2 < b->getX() + b->getW() / 2) &&
-         (a->getX() + a->getW() / 2 > b->getX() - b->getW() / 2) &&
-         (a->getY() - a->getH() / 2 <= b->getY() + b->getH() / 2) &&
-         (a->getY() + a->getH() / 2 >= b->getY() - b->getH() / 2)) {
+        (a->getX() + a->getW() / 2 > b->getX() - b->getW() / 2) &&
+        (a->getY() - a->getH() / 2 <= b->getY() + b->getH() / 2) &&
+        (a->getY() + a->getH() / 2 >= b->getY() - b->getH() / 2)) {
         return true;
     }
     return false;
